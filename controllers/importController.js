c5a736d86d65dc05b984231b5b8039cf4406e1ae
piumenotechnology <gerifitrah@gmail.com>
2025-05-16 02:55:15 +0800

    change server

--- controllers/importController.js
+++ controllers/importController.js
@@ -7,6 +7,9 @@ async function handleImport(req, res) {
     console.log('⚡ Starting Import...');
 
     const rawData = await fetchAllSheetsData(); // Fetch data from Google Sheets
+
+    console.log('data 1')
+
     const mappedData = mapSheetsData(rawData); // Map the data to the correct format
     await insertMappedData(mappedData); // Insert the mapped data into the database
 
