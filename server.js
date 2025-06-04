a395501265baa981c055902453c8fc4f962e239c
piumenotechnology <gerifitrah@gmail.com>
2025-06-04 22:28:28 +0800

    add

--- server.js
+++ server.js
@@ -33,7 +33,8 @@ app.get('/checkSheet', async(req, res)=> {
 
 app.post('/check_input', async(req, res) => {
   try {
-    const { session_id, message, table } = req.query;
+    // const { session_id, message, table } = req.query;
+    const { session_id, message, table } = req.body;
 
     console.log('🔍 Checking input parameters:', { session_id, message, table });
 
