429ae9d7a08de7070c7fe304875963547fdd8148
piumenotechnology <gerifitrah@gmail.com>
2025-07-04 02:16:31 +0800

    change cron into 21.30

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-cron.schedule('0 */6 * * *', async () => {
+cron.schedule('30 21 * * *', async () => {
   console.log('🔁 Running cron import task every 6 hours...');
   try {
     await runImportJob();
