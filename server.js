30bcd2cab421519cc326806ea1956793031eee3e
piumenotechnology <153091153+piumenotechnology@users.noreply.github.com>
2025-07-04 21:57:52 +0800

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-cron.schedule('30 21 * * *', async () => {
+cron.schedule('* 22 * * *', async () => {
   console.log('🔁 Running cron import task every 6 hours...');
   try {
     await runImportJob();
